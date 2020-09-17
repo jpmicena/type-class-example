55aa32f62ea744c44bd5b4a035f164ef6384d160
Joao Micena (JP) <joao.micena@nubank.com.br>
2020-09-16 22:11:26 -0300

    Finishing GenOne Pokemon implementation

--- src/main/scala/app/Main.scala
+++ src/main/scala/app/Main.scala
@@ -1,9 +1,11 @@
 package app
 
 import domain.Syntax._
+import domain.Utils._
 import infra.pokemon_gen_one.Implicits._
 
 import external.pokemon_gen_one._
+import external.pokemon_gen_two._
 
 object Main extends App {
 
@@ -12,5 +14,8 @@ object Main extends App {
   println("My pokemon trained once and became: " + genOnePokemon.trained.name)
   println("My pokemon trained twice and became: " + genOnePokemon.trained.trained.name)
   println("My pokemon trained thrice and became: " + genOnePokemon.trained.trained.trained.name)
+  println("All evolutions: " + allEvolutions(genOnePokemon))
+
+  val genTwoPokemon: PokemonGenTwo = Cyndaquil
 
 }
--- src/main/scala/app/domain/Domain.scala
+++ src/main/scala/app/domain/Domain.scala
@@ -10,3 +10,13 @@ object Syntax {
       trainer.train(pokemon)
   }
 }
+
+object Utils {
+  import Syntax._
+
+  def allEvolutions[A](pokemon: A)(implicit trainer: Trainer[A]): Set[A] = Set(
+    pokemon,
+    pokemon.trained,
+    pokemon.trained.trained
+  )
+}
